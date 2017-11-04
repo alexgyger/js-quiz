6f92f4d7f83aefa93e36005cddb2460828aa78f7
Alex Gyger <alexander.gyger38@gmail.com>
2017-11-04 10:15:59 +0100

    update content, preparation of external question file, new build

--- js/app.js
+++ js/app.js
@@ -10259,13 +10259,12 @@ let runningQuiz = false;
 let showingResults = false;
 let finishQuiz = false;
 let quizList = [
-  ['Are Java and JavaScript the same?', 'no']
-  ,
-  ['What was the profession of the famous Malevich?', 'painter'],
-  ['How many cantons does Switzerland have?', '26'],
-  ['"$(this)" is typical for which JavaScript library?', 'jquery'],
-  ['Which is the highest mountain in Switzerland?', 'dufourspitze']
- ];
+	["Which fish produces light to attract it's prey?", 'frogfish'],
+	['What does plankton eat?', 'algae'],
+	['How many years old did the oldest fish (a coi carp) become?', '226'],
+	['What is the largest fish in the world? (Two words)', 'whale shark'],
+	["How much in percent is the Earth's surface covered by oceans?", "70"]
+];
 
 function renderUI(message) {
   // DOM-Object
